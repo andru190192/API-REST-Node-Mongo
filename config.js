37d34e5ba4844e59dc69d4b7e45a91b714ecceaa
Andres Guachisaca <caguachisaca@gmail.com>
2017-09-12 23:02:24 -0500

    change config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+    db: 'ds135534.mlab.com:35534/heroku_94tmr3dl',
     SECRET_TOKEN: 'miclavedetokens'
 }
