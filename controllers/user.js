cb0c9154d0e402ca07db2541bcf13a5ae45a42fd
Andres Guachisaca <caguachisaca@gmail.com>
2017-09-15 11:35:08 -0500

    password user controller

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,8 @@ const service = require('../services')
 function signUp (req, res) {
     const user = new User({
         email: req.body.email,
-        displayName: req.body.displayName
+        displayName: req.body.displayName,
+        password: req.body.password
     })
 
     user.save((err) => {
